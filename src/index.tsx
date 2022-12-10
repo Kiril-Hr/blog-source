09cf92de43a62da4eddd33fe92a74894a50acbf8
Kiril <kirill.alekseevich.hr@gmail.com>
2022-12-10 21:26:31 +0200

    1

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+//JSX
+// const h1 = <h1>Hello world</h1>
+// Without JSX
+const h1 = React.createElement(
+   'h1',
+   { id: 'Title', className: 'text' },
+   'Hello React'
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{h1}</React.StrictMode>)
