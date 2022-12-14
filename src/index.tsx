79a8b20c69ae9008cd8e298ced7171b98a6a885c
Kiril <kirill.alekseevich.hr@gmail.com>
2022-12-14 22:14:19 +0200

    start project

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,9 @@
 import { title } from 'process'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Content from './Content'
-
-type TitleProps = {
-   title: string
-   text?: string
-}
-
-const Title = ({ title, text = 'test' }: TitleProps) => (
-   <h1>
-      {text} {title}
-   </h1>
-)
 
 const App = () => {
-   return (
-      <>
-         <Title title="React.js" text="Hello" />
-         <Content h1="About something" h2="About something else" />
-      </>
-   )
+   return <></>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
